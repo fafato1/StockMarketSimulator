1c08d18915bd8af8b50aefc696c7ee0a0ee04a50
Nathan <nathan.vinicius.dev@gmail.com>
2023-08-17 22:05:11 -0300

    - Loop na demonstração de valor - Aprendemos a mostrar quem usou o comando - Aprendemos a mencionar quem usou o comando!
    Co-authored-by: Luo <OJuNNiN@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import discord
+import asyncio
 from discord import app_commands
 from key import token
 TOKEN = token.get('TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
 
 id_do_servidor =  815714353793466448
 
@@ -9,20 +13,29 @@ class client(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.synced = False #Nós usamos isso para o bot não sincronizar os comandos mais de uma vez
-
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced: #Checar se os comandos slash foram sincronizados 
-            await tree.sync(guild = discord.Object(id=id_do_servidor)) # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
+            await clients.sync(guild = discord.Object(id=id_do_servidor)) # Você também pode deixar o id do servidor em branco para aplicar em todos servidores, mas isso fará com que demore de 1~24 horas para funcionar.
             self.synced = True
         print(f"Entramos como {self.user}.")
+        mercadao = aclient.get_channel(1141895078298783778)
+        while(self.synced == True):
+            mensagem = await mercadao.send("test")
+            await asyncio.sleep(10)
+            await mensagem.delete()
 
 aclient = client()
-tree = app_commands.CommandTree(aclient)
+clients = app_commands.CommandTree(aclient)
 
-@tree.command(guild = discord.Object(id=id_do_servidor), name = 'mercado', description='Inicia o sistema de mercado individualmente para você!') #Comando específico para seu servidor
-async def slash2(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Olá!", ephemeral = True) 
+@clients.command(guild = discord.Object(id=id_do_servidor), name = 'market', description='Visualiza como está o mercado!') #Comando específico para seu servidor
+async def market(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Olá!", ephemeral = False) 
+
+@clients.command(guild = discord.Object(id=id_do_servidor), name = 'balance', description='Mostra o seu saldo atual!') #Comando específico para seu servidor
+async def balance(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Olá {interaction.user.mention} Seu saldo é: ", ephemeral = False)
+
 
 
 aclient.run(TOKEN)
